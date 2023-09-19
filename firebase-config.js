e3c21d3cc3be3cdb6b9d31ae7c1416a9c43467ea
Saadinthehedhog <134141128+Saadinthehedhog@users.noreply.github.com>
2023-09-19 17:19:41 +0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAsG5RV8ZmDBBKM3bjp8T0Jbb8EKYF0EA4",
+    authDomain: "cyber-guardian-e43f6.firebaseapp.com",
+    projectId: "cyber-guardian-e43f6",
+    storageBucket: "cyber-guardian-e43f6.appspot.com",
+    messagingSenderId: "1029752690825",
+    appId: "1:1029752690825:web:c821962dc83f8bfa0c4e58"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
